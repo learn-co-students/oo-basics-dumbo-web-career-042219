d3abbcae82d25dc4ae7cefa6fc782e80ba6c020d
Kenton Archer <kenton8654@yahoo.com>
2019-04-26 12:50:17 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,33 @@
-# Make your shoe class here!
+require 'pry'
+class Shoe
+
+attr_reader :brand,:color,:size,:material,:condition
+attr_writer :color,:size,:material,:condition
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def color
+    @color
+  end
+
+  def size
+    @size
+  end
+
+  def material
+    @material
+  end
+
+
+  def cobble
+  puts "Your shoe is as good as new!"
+  # binding.pry
+  @condition = "new"
+
+
+  end
+
+
+
+end
